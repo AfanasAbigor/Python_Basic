d928d0a25537a28b41471524ade18d44daab0c56
Manas Sharma <61354715+AfanasAbigor@users.noreply.github.com>
2021-01-07 00:28:52 +0600

    Create Highest_number.py

---
+++ Highest_number.py
@@ -0,0 +1,22 @@
+list = input("Enter Numbers: ").split()
+
+n = int(len(list))
+
+temp = 0
+for i in range (0,n):
+    list[i] = int(list[i])
+    if list[i]>=temp:
+        temp = list[i]
+
+print(temp)
+
+"""
+list = input("Enter Numbers: ").split()
+temp = 0;
+
+for i in list:
+    tempo = int(i)
+    if tempo>=temp:
+        temp = tempo
+print(temp)
+"""
