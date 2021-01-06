86672764525dd542706c5dfd0d23ae79c51ad2a1
Manas Sharma <61354715+AfanasAbigor@users.noreply.github.com>
2021-01-06 16:04:16 +0600

    Update Random_Bill_Pay.py

--- Random_Bill_Pay.py
+++ Random_Bill_Pay.py
@@ -3,9 +3,14 @@ import random
 Seed = int(input("Enter Any Seed Number: "))
 random.seed(Seed)
 
-name_list = str(input("Enter Names Using comma ',' and Spaces ' '  : "))
+name_list = str(input("Enter Names Using comma ',' and Spaces ' '  : ")) # input names
 
-name = name_list.split(", ")
+name = name_list.split(", ") # splitting "," and " " from list and make it list
 
-person = random.choice(name)
-print(f"{person} is Going To Buy the Meal Today.")
+length = int(len(name) - 1) # length of the list
+
+ranDom = random.randint(0,length) 
+
+person = name[ranDom]
+
+print(f"{person} is Going to Buy the meal today.")
